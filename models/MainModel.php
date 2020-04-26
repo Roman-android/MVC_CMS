f7eb28f7fcc733b25f573ebd418532a4c13e584b
Roman Malkov <roman-li-83@yandex.ru>
2020-04-26 19:08:04 +0500

    Refactor "Controller.php", add class "MainView.php", that extends from class "View.php".

--- models/MainModel.php
+++ models/MainModel.php
@@ -7,6 +7,11 @@ use core\Model;
 
 class MainModel extends Model
 {
+    public function requestNews()
+    {
+        $result = $this->db->row("SELECT title,description FROM news");
+        return $result;
+    }
 
     public function requestMenu(){
         $menu = $this->db->row("SELECT item FROM menu");
